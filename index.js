ae1458fbc12d64e593fbe25c0f9aab62ba41d6a4
unknown <harriett.steed@gmail.com>
2023-05-05 00:06:45 -0600

    functions to write svg file

--- index.js
+++ index.js
@@ -1,5 +1,38 @@
+// importing package man
 const inquirer = require(`inquirer`);
 const fs = require(`fs`);
+// importing shapes
+const {square, triangle, circle} = require(`./lib/shapes.js`);
+
+// function using the fs module from the npm to use inquirer answers to make the logo
+function writeToFile(fileName, answers) {
+    let svgString="";
+    svgString = `<svg version="1.1" width="300" height="300" xmlns="http://www.w3.org/2000/svg">`;
+
+    // g tag applies same styling to children
+    svgString += "<g>";
+    svgString += `${answers.shape}`;
+
+    let shapeInput;
+    if (answers.shape === "square") {
+        shapeInput = new square();
+        svgString += `<rect x="73" y="40" width="160" height="160" fill="${answers.shapeColor}"/>`;
+    } else if (answers.shape === "triangle") {
+        shapeInput = new triangle();
+        svgString += `<polygon points="150, 40 73, 200 227,200" fill="${answers.shapeColor}"/>`;
+    } else if (answers.shape === "circle") {
+        shapeInput = new circle();
+        svgString += `<circle cx="150" cy="150" r="80" fill="${answers.shapeColor}"/>`;
+} 
+
+svgString += `<text x="150" y="130" text-anchor="middle" font-size="30" fill="${answers.textColor}">${answers.text}</text>`;
+svgString += "</g>";
+svgString += "</svg>";
+
+fs.writeFile(fileName, svgString, (err) => {
+    err ? console.log(err) : console.log(`Nice! Logo created!`);
+});
+}
 
 function promptUser() {
     inquirer.prompt ([
@@ -17,7 +50,7 @@ function promptUser() {
             type:`list`,
             name:`shape`,
             message:`Select a shape for the logo`,
-            choices: [`square`, `circle`, `triangle`],
+            choices: [`square`, `triangle`, `circle`],
         },
         {
             type: `input`,
